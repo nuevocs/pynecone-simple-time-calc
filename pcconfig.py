227bf2dbbd2a038ed6537a0c1df795f57ad0bb49
Tatsuya Tomita <tat@seriousexplosion.net>
2023-03-18 13:47:43 +0900

    bun

--- pcconfig.py
+++ pcconfig.py
@@ -4,7 +4,7 @@ import pynecone as pc
 config = pc.Config(
     app_name="pynecone_simple_time_calc",
     api_url="0.0.0.0:8000",
-    bun_path="/app/.bun/bin/bun",
+    bun_path="/pynecone_simple_time_calc/.bun/bin/bun",
     db_url="sqlite:///pynecone.db",
     env=pc.Env.DEV,
 )

a1e0367a9523fcb6f0d483843e919b8d60674b78
Tatsuya Tomita <tat@seriousexplosion.net>
2023-03-21 22:47:30 +0900

    env

--- pcconfig.py
+++ pcconfig.py
@@ -5,10 +5,10 @@ api_url = os.getenv('TEST_VAR')
 
 config = pc.Config(
     app_name="pynecone_simple_time_calc",
-    api_url="http://192.168.3.82:27210",
-    # api_url=f'"{api_url}"',
+    # api_url="http://192.168.3.82:27210",
+    api_url=f'"{api_url}"',
     bun_path="/app/.bun/bin/bun",
     db_url="sqlite:///pynecone.db",
-    telemetry_enabled=False,
+    # telemetry_enabled=False,
     # env=pc.Env.DEV,
 )

305fb6f1802f52a04dae48edb51b854510e7a967
Tatsuya Tomita <tat@seriousexplosion.net>
2023-03-23 10:55:14 +0900

    updated prod

--- pcconfig.py
+++ pcconfig.py
@@ -2,13 +2,15 @@ import os
 import pynecone as pc
 
 mode = os.environ.get('PC_APP_MODE')
+local_ip = os.environ.get('FASTAPI_URL_LOCAL_IP')
+fast_api_url = os.environ.get('FASTAPI_URL')
+fast_api_port = os.environ.get('PORTS_API')
 
 if mode == "PROD":
-    fast_api_url = os.environ.get('FASTAPI_URL')
-
     config = pc.Config(
         app_name="pynecone_simple_time_calc",
-        api_url=f"https://{fast_api_url}",
+        # api_url=f"https://{fast_api_url}",
+        api_url=f"http://{local_ip}:{fast_api_port}",
         bun_path="/app/.bun/bin/bun",
         db_url="sqlite:///pynecone.db",
         # telemetry_enabled=False,
@@ -16,12 +18,9 @@ if mode == "PROD":
     )
 
 elif mode == "DEV":
-    fast_api_url = os.environ.get('FASTAPI_URL')
-    fast_api_port = os.environ.get('PORTS_API')
-
     config = pc.Config(
         app_name="pynecone_simple_time_calc",
-        api_url=f"http://{fast_api_url}:{fast_api_port}",
+        api_url=f"http://{local_ip}:{fast_api_port}",
         bun_path="/app/.bun/bin/bun",
         db_url="sqlite:///pynecone.db",
         # telemetry_enabled=False,
